1888c6dc7e0a14d9aeef22de779bb557c6acc5cd
Henrique <henscalon@hotmail.com>
2017-08-13 20:25:09 -0300

    added http server

--- main.go
+++ main.go
@@ -3,9 +3,15 @@ package main
 import (
 	"fmt"
 	"encoding/xml"
+	"encoding/json"
 	"net/http"
 	"bytes"
 	"io/ioutil"
+	"log"
+	"strings"
+	"sync"
+
+    "github.com/gorilla/mux"
 )
 
 /* Example request
@@ -34,27 +40,27 @@ const SOAP_ENVELOPE = `<?xml version="1.0" encoding="UTF-8"?>
 </SOAP-ENV:Envelope>`
 
 type Evento struct {
-	Data string `xml:"data"`
-	Hora string `xml:"hora"`
-	Descricao string `xml:"descricao"`
+	Data string `xml:"data" json:"data"`
+	Hora string `xml:"hora" json:"hora"`
+	Descricao string `xml:"descricao" json:"descricao"`
 }
 
 type Objeto struct {
-	Numero string `xml:"numero"`
-	Erro string `xml:"erro"`
-	Nome string `xml:"nome"`
-	Evento Evento `xml:"evento"`
+	Numero string `xml:"numero" json:"numero"`
+	Erro string `xml:"erro" json:"erro"`
+	Nome string `xml:"nome" json:"nome"`
+	Evento Evento `xml:"evento" json:"evento"`
 }
 
 type SoapResponse struct {
 	XMLName xml.Name
-	Objeto Objeto `xml:"return>objeto"`
+	Objeto Objeto `xml:"return>objeto" json:"objeto"`
 }
 
 type SoapFault struct {
-	Faultcode string `xml:"faultcode"`
-	Faultstring string `xml:"faultstring"`
-	Detail string `xml:"detail"`
+	Faultcode string `xml:"faultcode" json:"faultcode"`
+	Faultstring string `xml:"faultstring" json:"faultstring"`
+	Detail string `xml:"detail" json:"detail"`
 }
 
 type SoapBody struct {
@@ -67,8 +73,8 @@ type SoapEnvelope struct {
 	Body SoapBody
 }
 
-func GetSoapEnvelope(codigoRastreamento string) (*SoapEnvelope, error) {
-	soapRequestContent := fmt.Sprintf(SOAP_ENVELOPE, codigoRastreamento)
+func GetSoapEnvelope(trackingNumber string) (*SoapEnvelope, error) {
+	soapRequestContent := fmt.Sprintf(SOAP_ENVELOPE, trackingNumber)
 	httpClient := new(http.Client)
 	resp, err := httpClient.Post(SOAP_URL, "text/xml; charset=utf-8", bytes.NewBufferString(soapRequestContent))
 	if err != nil {
@@ -89,34 +95,84 @@ func GetSoapEnvelope(codigoRastreamento string) (*SoapEnvelope, error) {
 	return envelope, nil
 }
 
-func consultarCodigosRastreamento(codigosRastreamento []string) {
-	codigos := make(chan bool, len(codigosRastreamento))
+func respondWithError(w http.ResponseWriter, code int, message string) {
+    respondWithJSON(w, code, map[string]string{"error": message})
+}
 
-	for _, cod := range codigosRastreamento {
-		go func(codigoRastreamento string) {
-			env, err := GetSoapEnvelope(codigoRastreamento)
-			if err == nil {
-				fmt.Printf("Numero: %s\n Erro: %s Evento: %v\n",
-					env.Body.Response.Objeto.Numero,
-					env.Body.Response.Objeto.Erro,
-					env.Body.Response.Objeto.Evento)
-			} else {
-				fmt.Printf("Erro ao obter codigo de rastreamento %s: %s\n", codigoRastreamento, err)
-			}
-			codigos <- true
-		}(cod)
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+    response, _ := json.Marshal(payload)
+
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(code)
+    w.Write(response)
+}
+
+type RetTrackingNumbers struct {
+	Objetos []Objeto
+}
+
+func doGetTrackingNumbers(trackingNumbers []string) (*RetTrackingNumbers, error) {
+	ret := new(RetTrackingNumbers)
+
+	length := len(trackingNumbers)
+	if length > 0 {
+		codes := make(chan bool, length)
+
+		var err error
+		mutexRet := &sync.Mutex{}
+		mutexErr := &sync.Mutex{}
+		for _, cod := range trackingNumbers {
+			go func(trackingNumber string) {
+				var errSoap error
+				if err == nil {
+					var env *SoapEnvelope
+					env, errSoap = GetSoapEnvelope(trackingNumber)
+					if errSoap == nil {
+						if env.Body.Fault.Faultstring != "" {
+							errSoap = fmt.Errorf("Faultcode: %s. Faultstring: %s. Detail: %s", env.Body.Fault.Faultcode, env.Body.Fault.Faultstring, env.Body.Fault.Detail)
+						} else {
+							mutexRet.Lock()
+							ret.Objetos = append(ret.Objetos, env.Body.Response.Objeto)
+							mutexRet.Unlock()
+						}
+					}
+				}
+				mutexErr.Lock()
+				err = errSoap
+				mutexErr.Unlock()
+				codes <- true
+			}(cod)
+		}
+
+		for i := 0; i < length; i++ {
+			<-codes
+		}
+
+		if err != nil {
+			return nil, err
+		}
 	}
 
-	for i := 0; i < len(codigosRastreamento); i++ {
-		<-codigos
+	return ret, nil
+}
+
+func GetTrackingNumbers(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	trackingNumbers := strings.Split(strings.TrimSpace(vars["tracking_numbers"]), ";")
+
+	ret, err := doGetTrackingNumbers(trackingNumbers)
+
+	if err != nil {
+		log.Printf("GetTrackingNumbers: %v\n", err.Error())
+		respondWithError(w, http.StatusInternalServerError,  "Error retrieving tracking numbers.")
+	} else {
+		respondWithJSON(w, http.StatusOK, ret);
 	}
 }
 
 func main() {
-	codigosRastreamento := []string{
-		"XFASDASDXX",
-		"cvaSDF23123",
-	}
+    router := mux.NewRouter()
+    router.HandleFunc("/tracking_numbers/{tracking_numbers}", GetTrackingNumbers).Methods("GET")
 
-	consultarCodigosRastreamento(codigosRastreamento)
+    log.Fatal(http.ListenAndServe(":8000", router))
 }
